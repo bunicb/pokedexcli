f5ef5d276bbdf33f590d0e0b93daf9c5bc274363
Bojan Bunić <bojan@bunic.info>
2025-08-24 10:27:48 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	input := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex> ")
-		if !input.Scan() {
-			break
-		}
-		line := input.Text()
-		words := cleanInput(line)
-		fmt.Println("Your command was:", words[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	"strings"
+	"fmt"
+	"bufio"
+	"os"
 )
+
+func startRepl() {
+	input := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex> ")
+		input.Scan()
+
+		words := cleanInput(input.Text())
+		if len(words) == 0 {
+			continue
+		}
+		
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
