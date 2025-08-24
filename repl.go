c0e464477f8dca06d47c869264b27792aa0c43d8
Bojan Bunić <bojan@bunic.info>
2025-08-24 11:22:02 +0200

    implement commands

--- repl.go
+++ repl.go
@@ -7,6 +7,41 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand = make(map[string]cliCommand)
+
+func init() {
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
 func startRepl() {
 	input := bufio.NewScanner(os.Stdin)
 	for {
@@ -18,7 +53,17 @@ func startRepl() {
 			continue
 		}
 		
-		fmt.Printf("Your command was: %s\n", words[0])
+		userCommand := words[0]
+
+		validCommand, ok := commands[userCommand]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := validCommand.callback()
+		if err != nil {
+			fmt.Println("Error executing command:", err)
+		}
 	}
 }
 
