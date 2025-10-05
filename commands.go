cf57f0e029bffeced172deca2cc8c228db201314
Bojan Bunić <bojan@bunic.info>
2025-10-05 16:42:31 +0200

    inspect pokemon

--- commands.go
+++ commands.go
@@ -40,6 +40,11 @@ func getCommands() map[string]cliCommand {
 			description: "Catch a pokemon and add to pokedex",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "Inspect a caught pokemon",
+			callback:    commandInspect,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
@@ -132,5 +137,29 @@ func commandCatch(cfg *config, args ...string) error {
 	}
 	fmt.Printf("%s was caught!\n", pokemon.Name)
 	cfg.caughtPokemon[pokemon.Name] = pokemon
+	return nil
+}
+
+func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("please provide a pokemon name")
+	}
+	name := args[0]
+	pokemon, ok := cfg.caughtPokemon[name]
+	if !ok {
+		fmt.Println("you have not caught that pokemon")
+		return nil
+	}
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf(" - %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, t := range pokemon.Types {
+		fmt.Printf(" - %s\n", t.Type.Name)
+	}
 	return nil
 }
